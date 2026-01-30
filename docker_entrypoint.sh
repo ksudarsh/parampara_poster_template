68ad0812be84e5aeb7e08ef389b49dcbbd551041
ksudarsh <ksudarsh@users.noreply.github.com>
2026-01-30 22:18:10 +0530

    Copy generated posters to /out

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -7,7 +7,7 @@ python generate_parampara_poster.py
 out_dir="/out"
 if [ -d "$out_dir" ]; then
   shopt -s nullglob
-  files=(Sri_Parakala_Matham_Guru_Parampara_GRID_*.pdf Sri_Parakala_Matham_Guru_Parampara_GRID_*.png)
+  files=(Sri_Parakala_Matham_Guru_Parampara_*.pdf Sri_Parakala_Matham_Guru_Parampara_*.png)
   if [ ${#files[@]} -gt 0 ]; then
     cp -f "${files[@]}" "$out_dir/"
     echo "Copied output files to $out_dir"
